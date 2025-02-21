82980683dc7d904eb1611877ac597a3e841bf2d1
Rsabha107 <61208207+Rsabha107@users.noreply.github.com>
2025-02-21 11:08:31 +0300

    init 1

---
+++ resources/views/emails/sample.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+
+    <!--
+	You can put your custom CSS if you wish
+    -->
+</head>
+<body>
+    <p>Dear {{ $content['name'] }}</p>
+    <p>{{ $content['body'] }}</p>
+    <p>Start Date: {{ $content['start_date'] }}</p>
+    <p>Due Date: {{ $content['due_date'] }}</p>
+    <p>Task Description: </p>
+    <p>{{ $content['description']}}</p>
+    <p> Let's finish meshwarna! </p>
+</body>
+</html>
